c85b10d9f62388dad717ba0a7674f73007a04337
Glider <2929799602@qq.com>
2023-02-09 17:10:11 +0800

    feat: [alt-tab] electron 内置方法获取图标

--- packages/main/src/alt-tab/system.ts
+++ packages/main/src/alt-tab/system.ts
@@ -1,4 +1,6 @@
+import { app } from 'electron'
 import { getAllInfo, toggleWindow } from './windows'
+import type { WindowAltTabTaskItem } from './type'
 
 // Q-A: 导入ffi-napi 后 打包出来的文件 electron 执行后直接退出
 // A: ArrayBuffer backing stores must be allocated inside the sandbox address space
@@ -8,9 +10,24 @@ import { getAllInfo, toggleWindow } from './windows'
 // A: user32 中函数名称导入错误
 export const getAltTabTask = async () => {
   const altTabTaskList = await getAllInfo()
+  await getProgressTaskIcon((altTabTaskList as Array<WindowAltTabTaskItem>) || [])
   return altTabTaskList
 }
 
 export const toggleThisWindows = (appHwnd: string) => {
   toggleWindow(appHwnd)
+}
+
+const getProgressTaskIcon = async (taskListArr: Array<WindowAltTabTaskItem>) => {
+  const iconSet = new Set()
+  for (let i = 0; i < taskListArr.length; i++) {
+    const currentTaskList = taskListArr[i]
+    if (iconSet.has(currentTaskList.processName)) continue
+
+    currentTaskList.appIcon = (
+      await app.getFileIcon(currentTaskList.processName, {
+        size: 'large'
+      })
+    )?.toDataURL()
+  }
 }
---
+++ packages/main/src/lib/window/shellapi/index.ts
@@ -0,0 +1,18 @@
+import ffi from 'ffi-napi'
+import { windowType } from '../index'
+
+const { Def, BOOL, POINTER, LONG_PTR, HANDLE } = windowType
+const libUser32 = ffi.Library('user32', {
+  EnumWindows: [BOOL, [POINTER, LONG_PTR]],
+  GetShellWindow: [POINTER, []],
+  GetWindowLongA: [HANDLE, [HANDLE, Def.int]],
+  GetAncestor: [HANDLE, [HANDLE, Def.uint]],
+  GetLastActivePopup: [HANDLE, [HANDLE]],
+  IsWindowVisible: [BOOL, [HANDLE]],
+  GetWindowTextA: [Def.int, [HANDLE, Def.uint16Ptr, Def.int]],
+  GetWindowTextLengthA: [Def.int, [Def.int]],
+  GetWindowThreadProcessId: [Def.int, [HANDLE, Def.uint16Ptr]],
+  GetWindowModuleFileNameA: ['int', [HANDLE, 'uchar*', 'int']]
+})
+
+export default libUser32
